050322e9aa3801ccf3d644f473a452aaeac4e540
Sebastian Bechtel <me@sebastian-bechtel.info>
2010-03-28 20:39:15 +0200

    renaming

---
+++ _testdata/sample.php
@@ -0,0 +1,20 @@
+<?php
+class Foo {
+    public function Bar() {
+	$str = "Hello";
+	$str2 = "World";
+
+	print $str . " " . $str2;
+
+    }
+
+    public function Bat() {
+
+    }
+}
+
+class A {
+    public function B() {
+	print "Hello World!";        
+    }
+}
